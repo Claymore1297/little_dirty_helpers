f35b9e9cf63f76b05bf0a947a1aefe367b329afa
Julian Veit <claymore1298@gmail.com>
2026-04-09 22:56:06 +0200

    bash: EdgeTX: setenv.sh: add automatic toolchain download

--- bash/EdgeTX/setenv.sh
+++ bash/EdgeTX/setenv.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-#! /bin/bash
 # Copyright (C) 2026 Claymore1297 / Julian Veit
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +13,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+TOOLSCHAIN_BASE="/home/julian/git_Sandbox/edgetx/tools"
+[ ! -d "$TOOLSCHAIN_BASE" ] && mkdir "$TOOLSCHAIN_BASE"
 
-TOOLSCHAIN="/home/julian/git_Sandbox/edgetx/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/bin"
-export PATH=$TOOLSCHAIN:$PATH
+# version definition
+TOOLCHAIN_VERSION="14.2.rel1"
+
+# ===== PATHS =====
+TOOLCHAIN_DIR="$TOOLSCHAIN_BASE/arm-gnu-toolchain-$TOOLCHAIN_VERSION-x86_64-arm-none-eabi"
+TOOLCHAIN_BIN="$TOOLCHAIN_DIR/bin"
+
+# ===== INSTALL TOOLCHAIN =====
+if [ ! -d "$TOOLCHAIN_DIR" ]; then
+    echo ">>> Downloading ARM toolchain... (${TOOLSCHAIN_VERSION})"
+    cd "$TOOLSCHAIN_BASE"
+    wget https://developer.arm.com/-/media/Files/downloads/gnu/$TOOLCHAIN_VERSION/binrel/arm-gnu-toolchain-$TOOLCHAIN_VERSION-x86_64-arm-none-eabi.tar.xz
+    tar -xf arm-gnu-toolchain-$TOOLCHAIN_VERSION-*.tar.xz
+fi
+
+export PATH=$TOOLCHAIN_BIN:$PATH
 
